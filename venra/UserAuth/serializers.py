d47be17de1f0b0db62dba2713d9f24467966a5fb
Andre-Ovien <andreovien571@gmail.com>
2025-09-11 18:25:06 +0100

    serializers for login

--- venra/UserAuth/serializers.py
+++ venra/UserAuth/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import User
-
+from django.contrib.auth import authenticate
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
@@ -15,3 +15,19 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if User:
+            user = authenticate(email=email, password=password)
+            if not user:
+                raise serializers.ValidationError('account does not exist')
+        attrs['user'] = user
+        return attrs
