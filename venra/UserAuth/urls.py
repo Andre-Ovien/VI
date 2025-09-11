7cb277a35153f128321feea9ab3486b51496c720
Andre-Ovien <andreovien571@gmail.com>
2025-09-11 18:25:38 +0100

    endpoint for login

--- venra/UserAuth/urls.py
+++ venra/UserAuth/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('api/user/register',views.RegisterUserView.as_view(), name="register")
+    path('api/user/register',views.RegisterUserView.as_view(), name="register"),
+    path('api/auth',views.LoginUserView.as_view(), name="login"),
 ]
