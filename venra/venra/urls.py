531a9274edbd9a9398ede1b63f69ac03605bc1bb
Andre-Ovien <andreovien571@gmail.com>
2025-09-12 18:06:24 +0100

    update

--- venra/venra/urls.py
+++ venra/venra/urls.py
@@ -5,6 +5,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('UserAuth.urls')),
+    path('students/', include('Courses.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name="token_obtain"),
     path('api/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
 ]
